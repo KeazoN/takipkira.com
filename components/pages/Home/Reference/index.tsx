6fcfb90c31b57720b74483a871d0d16ed1ffed17
Uğur Mercan <keazonoffical@gmail.com>
2025-03-10 15:34:21 +0300

    Update Reference section with new layout, text, and call-to-action button

--- components/pages/Home/Reference/index.tsx
+++ components/pages/Home/Reference/index.tsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
+import { TbArrowRight, TbLogin } from "react-icons/tb";
 
 const Reference = () => {
   return (
     <section>
-      <div className="container flex items-center justify-center flex-col gap-10 max-w-[1450px]">
-        <div className="flex items-center justify-center flex-col gap-2">
-          <h2 className="text-4xl font-extrabold">
-            Bize güvendiler, güvenmeye{" "}
-            <span className="text-primary">devam ediyorlar!</span>
-          </h2>
-          <p className="text-gray-500">
-            İşletmelerinizin güvenliğini sağlamak için güvenilir bir partner
-            arıyorsanız, bizimle iletişime geçin.
-          </p>
+      <div className="container flex items-center justify-center flex-col gap-10 max-w-[1255px]">
+        <div className="flex items-center justify-between w-full">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-4xl font-extrabold">
+              Onlarla Çalışıyoruz,{" "}
+              <span className="text-primary">Gücünüze Güç Katıyoruz!</span>
+            </h2>
+            <p className="text-gray-500">
+              İşletmelerinizin güvenliğini sağlamak için güvenilir bir partner
+              arıyorsanız, bizimle iletişime geçin.
+            </p>
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <button className="bg-primary text-white px-8 shadow-lg py-2 rounded-3xl group flex items-center gap-2 transition hover:brightness-75">
+              Hemen Başla
+              <TbArrowRight className="w-5 h-5 group-hover:translate-x-1 transition" />
+            </button>
+          </div>
         </div>
         <div className="overflow-hidden whitespace-nowrap relative">
           <div className="w-[1000px] relative">
