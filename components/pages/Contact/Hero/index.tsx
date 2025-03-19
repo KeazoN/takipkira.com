614fc9c26a9e6dc52cdd73c1953241ae42f2b9de
Uğur Mercan <keazonoffical@gmail.com>
2025-03-19 12:56:31 +0300

    Add Contact page with dynamic Hero section featuring rotating text and call-to-action buttons; enhance Home page with updated messaging for improved user engagement

--- components/pages/Contact/Hero/index.tsx
+++ components/pages/Contact/Hero/index.tsx
@@ -8,13 +8,9 @@ import { TbArrowRight } from "react-icons/tb";
 
 const Hero = () => {
   return (
-    <section className="container mt-4 flex items-center justify-center max-w-[1450px] relative overflow-hidden w-full gap-20 rounded-3xl h-[650px] bg-gradient-to-bl from-sky-50 to-sky-100">
-      <div className="flex flex-col items-start gap-6 w-[40%]">
-        <span className="bg-sky-200 font-bold p-2 pr-4 text-sm flex items-center justify-center gap-2 rounded-full">
-          <span className="px-2 py-1 bg-white rounded-full">Hesapla</span>
-          Konut ve Arsa Kredisi Hesaplamak İçin Tıklayın!
-        </span>
-        <h1 className="flex flex-col text-nowrap text-5xl transition-all duration-300 font-extrabold gap-2">
+    <section className="container mt-4 flex items-center justify-center max-w-[1450px] relative overflow-hidden w-full gap-20 rounded-3xl h-[500px] bg-gradient-to-bl from-sky-50 to-sky-100">
+      <div className="flex flex-col items-center justify-center mt-16 gap-6 w-[40%]">
+        <h1 className="flex flex-col text-nowrap text-5xl  transition-all duration-300 font-extrabold gap-2">
           Takip Kira ile Kolayı Var
           <RotatingText
             texts={[
@@ -35,16 +31,7 @@ const Hero = () => {
             rotationInterval={4000}
           />
         </h1>
-
-        {/* <SplitText
-          text="Mülklerinizin Bütün İşlerini Bize Bırakın!"
-          className="text-5xl font-extrabold"
-          delay={50}
-          animationFrom={{ opacity: 0, transform: "translate3d(0,40px,0)" }}
-          animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-          textAlign="left"
-        /> */}
-        <p className="text-lg opacity-70 leading-relaxed">
+        <p className="text-lg opacity-70 text-center leading-relaxed">
           Takip Kira, kira takip sistemidir. Kira takip sistemini kullanarak
           kira takipini kolaylaştirin.
         </p>
@@ -57,28 +44,6 @@ const Hero = () => {
             Daha Fazla{" "}
             <TbArrowRight className="w-4 h-4 group-hover:translate-x-1 transition" />
           </button>
-        </div>
-        <div className="grid grid-cols-3 gap-4 absolute bottom-24">
-          <PlayStore />
-          <AppStore />
-          <AppGallery />
-        </div>
-      </div>
-      <div className="relative">
-        <Image
-          src="/assets/img/phone/mockup.png"
-          alt="mockup"
-          width={600}
-          height={600}
-          className="relative top-16 drop-shadow-lg z-20"
-        />
-        <div className="absolute top-20 z-0 -right-16 w-full h-full">
-          <Image
-            src="/assets/blobs/blobs.svg"
-            alt="blobs"
-            width={1000}
-            height={1000}
-          />
         </div>
       </div>
     </section>
---
+++ components/pages/Contact/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+
+const Contact = () => {
+  return (
+    <div className="flex flex-col gap-10 mb-20">
+      <Hero />
+    </div>
+  );
+};
+
+export default Contact;
