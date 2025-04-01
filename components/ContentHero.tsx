0c4511e81a86c9c8ccdd1114284bd4cac473f17c
Uğur Mercan <keazonoffical@gmail.com>
2025-04-01 21:52:53 +0300

    Enhance ContentHero component with responsive design adjustments; update layout and typography for better mobile compatibility, including changes to margins, widths, and text sizes for improved user experience across devices.

--- components/ContentHero.tsx
+++ components/ContentHero.tsx
@@ -8,15 +8,15 @@ const ContentHero = ({
   description: string;
 }) => {
   return (
-    <section className="container mt-4 flex items-center justify-center max-w-[1450px] relative overflow-hidden w-full gap-20 rounded-3xl h-[370px] bg-gradient-to-bl from-sky-50 to-sky-100">
-      <div className="flex flex-col items-center justify-center mt-16 gap-6 w-[40%]">
-        <h1 className="flex flex-col text-nowrap text-5xl  text-center items-center justify-center transition-all duration-300 font-extrabold gap-2">
+    <section className="container mt-4 flex items-center justify-center max-w-[1450px] relative overflow-hidden w-full gap-20 rounded-3xl h-[370px] max-lg:h-fit max-lg:py-16 bg-gradient-to-bl from-sky-50 to-sky-100">
+      <div className="flex flex-col items-center justify-center mt-16 max-lg:mt-0 gap-6 w-[40%] max-lg:w-[90%] max-sm:w-full max-sm:px-4 max-md:relative max-md:top-8">
+        <h1 className="flex flex-col text-nowrap max-lg:text-wrap text-5xl max-lg:text-4xl max-sm:text-3xl text-center items-center justify-center transition-all duration-300 font-extrabold gap-2">
           {title}
-          <span className="rounded-lg bg-primary text-center text-white px-4 py-2">
+          <span className="rounded-lg bg-primary text-center text-white px-4 py-2 max-sm:px-3 max-sm:py-1.5">
             {title2}
           </span>
         </h1>
-        <p className="text-lg opacity-70 text-center leading-relaxed">
+        <p className="text-lg max-lg:text-base opacity-70 text-center leading-relaxed max-sm:text-sm">
           {description}
         </p>
       </div>
